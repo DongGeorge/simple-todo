3d0f8dd4ac38aaefaaa724797b33c81a8b38ae79
dong.george03@gmail.com <gdong37@gatech.edu>
2025-07-14 16:35:29 -0400

    Turned add button to a form

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,32 @@ import Icon from '@mdi/react'
 function App() {
   const addButtonStyle = {
     "display": "flex",
-    "align-items": "center"
+    "align-items": "center",
+    'height': '60px',
+    'gap': '10px'
   }
+
+  const inputStyle = {
+    'height':'60%',
+    'width': '200px'
+  }
+
+  function addToList(formData) {
+    const newTask = formData.get('task')
+    console.log(newTask)
+  }
+
   return (
     <>
-      <div>
+      <form style={addButtonStyle} action={addToList}>
+        <input type="text" name='task' style={inputStyle}/>
         <button style={addButtonStyle}>
           <Icon path={mdiPlusCircle} size={1}></Icon>
-          <p style={{"margin-left": "10px"}}>Add Task</p>
+          <p>Add Task</p>
         </button>
-      </div>
+      </form>
+
+      
     </>
   )
 }
