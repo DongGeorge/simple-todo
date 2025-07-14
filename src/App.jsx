53d152613f3504c088ff2e835a10a05e6597e8bc
dong.george03@gmail.com <gdong37@gatech.edu>
2025-07-14 16:20:29 -0400

    Added basic prompt button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import Task from './Components/Task'
+import { mdiPlusCircle } from '@mdi/js'
+import Icon from '@mdi/react'
 
 function App() {
+  const addButtonStyle = {
+    "display": "flex",
+    "align-items": "center"
+  }
   return (
     <>
-      <Task />
+      <div>
+        <button style={addButtonStyle}>
+          <Icon path={mdiPlusCircle} size={1}></Icon>
+          <p style={{"margin-left": "10px"}}>Add Task</p>
+        </button>
+      </div>
     </>
   )
 }
